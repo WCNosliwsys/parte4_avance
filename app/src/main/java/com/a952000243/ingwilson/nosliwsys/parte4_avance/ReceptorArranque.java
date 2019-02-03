06f862ccb397f81348602d47c029fa3c14625500
Ing Wilson <nosliwsys@gmail.com>
2019-02-03 15:17:20 -0500

    Receptor de anuncios servicios y notificaciones fin

--- app/src/main/java/com/a952000243/ingwilson/nosliwsys/parte4_avance/ReceptorArranque.java
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/parte4_avance/ReceptorArranque.java
@@ -1,5 +1,9 @@
 package com.a952000243.ingwilson.nosliwsys.parte4_avance;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 public class ReceptorArranque  extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
